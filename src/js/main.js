8d779aea04dbb07e1fb7740f6d625279bca6a518
Mikołaj Rodak <mrodak03@gmail.com>
2023-08-20 20:50:03 +0200

    added nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
-const h1 = document.querySelector('h1')
+const nav = document.querySelector('.nav')
+const navBtn = document.querySelector('.burger-btn')
+const navItem = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
+
+	if (nav.classList.contains('nav--active')) {
+		navBtnBars.classList.add('black-bars-color')
+	} else {
+		navBtnBars.classList.remove('black-bars-color')
+	}
+
+	navItem.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+
+	handleNavItemsAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	navItem.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+navBtn.addEventListener('click', handleNav)
